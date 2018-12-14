7a76a7c9321a3b4f5cc1b5cbcf78a492f0ca1849
adaildoneto <adaildo.neto@gmail.com>
2018-12-14 13:50:00 -0500

    card

--- cardnews.php
+++ cardnews.php
@@ -9,7 +9,7 @@
 
         </div>
         <div class="card-image">
-                      <?php $image =  wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'card-news' );?>
+                      <?php $image =  wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'cardnews' );?>
           <img class="responsive-img" src="<?php echo $image[0]; ?>" >
 
             </div>
