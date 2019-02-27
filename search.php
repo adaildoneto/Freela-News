0ce43cfc52d4cc4c185614abaa120a62c118103c
adaildoneto <adaildo.neto@gmail.com>
2019-02-26 19:36:29 -0500

    ok

--- search.php
+++ search.php
@@ -13,7 +13,7 @@ get_header(); ?>
 				<?php if ( have_posts() ) : ?>
 
 					<header class="page-header">
-						<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'odin' ), get_search_query() ); ?></h1>
+						<h4 class="page-title"><?php printf( __( 'Search Results for: %s', 'odin' ), get_search_query() ); ?></h4>
 					</header><!-- .page-header -->
 
 						<?php
