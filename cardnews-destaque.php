bb39c5fc13390a381178fe3c8b40720bbc3e440b
adaildoneto <adaildo.neto@gmail.com>
2019-02-26 22:01:25 -0500

    agora vai

--- cardnews-destaque.php
+++ cardnews-destaque.php
@@ -1,11 +1,12 @@
   <?php $image =  wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'big-quadrado' );?>
   <?php if( !empty( $image ) ): ?>
-<div class="card destaque-post teal darken-4">
-<div class="center-align">
-<?php else :?>
 
-<div class="card destaque-post" style="background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0) 52%,rgba(0,0,0,0.65) 84%,rgba(0,0,0,0.65) 100%), url('<?php echo $image[0]; ?>'); background-size: cover; background-position: center;">
-<div class="card-content">
+  <div class="card destaque-post" style="background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0) 52%,rgba(0,0,0,0.65) 84%,rgba(0,0,0,0.65) 100%), url('<?php echo $image[0]; ?>'); background-size: cover; background-position: center;">
+  <div class="card-content">
+<?php else :?>
+  <div class="card destaque-post teal darken-4">
+  <div class="center-align">
+
 <?php endif ?>
 
 
