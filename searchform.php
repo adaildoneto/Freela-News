d77cfcefb28fe058939560b6830d5a0ff9ce1450
adaildoneto <adaildo.neto@gmail.com>
2019-02-28 13:04:30 -0500

    pesquisa

--- searchform.php
+++ searchform.php
@@ -7,14 +7,14 @@
  */
 ?>
 
-<div class="row">
-	<div class="nav-wrapper">
-				<form>
-					<div class="input-field">
-					 <input id="search" type="search" placeholder="Pesquisa" value="<?php echo get_search_query(); ?>" name="s"/>
-					 <label class="label-icon active" for="search"><i class="material-icons right">search</i></label>
-					 <i class="material-icons">close</i>
-					 </div>
-				 </form>
+<nav>
+	<div class="nav-wrapper teal darken-3">
+		<form>
+			<div class="input-field">
+			 <input id="search" type="search" placeholder="Pesquisa" value="<?php echo get_search_query(); ?>" name="s"/>
+				<label class="label-icon" for="search"><i class="material-icons">search</i></label>
+				<i class="material-icons">close</i>
+			</div>
+		</form>
 	</div>
-</div>
+</nav>
