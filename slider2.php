2428f93f60c1d6983ccf78e9fcb83c07cbddb0df
adaildoneto <adaildo.neto@gmail.com>
2019-02-28 10:35:12 -0500

    v

--- slider2.php
+++ slider2.php
@@ -2,8 +2,8 @@
         <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider' );?>
       <div class="card slider-post" style="background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0) 52%,rgba(0,0,0,0.65) 84%,rgba(0,0,0,0.65) 100%), url('<?php echo $image[0]; ?>'); background-size: cover; background-position: center;">
             <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
-            <h5 class="white-text" style="position: absolute; bottom: 20px; padding: 10px;">
+            <b><h6 class="white-text" style="position: absolute; bottom: 20px; padding: 10px; text-shadow: 4px 4px 2px rgba(0, 0, 0, 0.7);">
               <?php the_title();?>
-            </h5>  </a>
+            </h6></b>  </a>
           </div>
 </div>
