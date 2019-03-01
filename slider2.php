470e6d79bb8b47f77b581061a801b67e0647d6ef
adaildoneto <adaildo.neto@gmail.com>
2019-02-28 19:04:00 -0500

    ok

--- slider2.php
+++ slider2.php
@@ -1,4 +1,5 @@
 <div style="margin: 20px;">
+    <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
         <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider' );?>
       <div class="card slider-post" style="background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0) 52%,rgba(0,0,0,0.65) 84%,rgba(0,0,0,0.65) 100%), url('<?php echo $image[0]; ?>'); background-size: cover; background-position: center;">
             <div style="position: absolute; bottom: 20px; padding: 10px; ">
@@ -8,15 +9,16 @@
                             echo '<span class="new badge teal darken-4" data-badge-caption="'. esc_html( $categories[0]->name ) . '"></span>';
                           }?>
                   </div>
-                  <div class="">
-                    <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
-                    <b><h5 class="white-text" style="text-shadow: 4px 4px 2px rgba(0, 0, 0, 0.7);">
+                  <div class="news-hgrande white-text">
+
+                    <b><span class="white-text" style="text-shadow: 4px 4px 2px rgba(0, 0, 0, 0.7);">
                       <?php the_title();?>
-                    </h5></b>  </a>
+                    </span></b>
                   </div>
 
             </div>
 
 
           </div>
+           </a>
 </div>
