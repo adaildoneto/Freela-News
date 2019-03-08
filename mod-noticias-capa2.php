974702da5f2b505b0ad489c85b5e1d10bffee875
adaildoneto <adaildo.neto@gmail.com>
2019-03-07 21:41:19 -0500

    orringindo as cores

--- mod-noticias-capa2.php
+++ mod-noticias-capa2.php
@@ -1,4 +1,4 @@
-<div class="row teal lighten-3"  style="padding: 20px;">
+<div class="row grey lighten-3"  style="padding: 20px;">
 	<div class="container">
 		<div class="center-align titulo">
 			<h5 class="white-text">Colunas</h5>
